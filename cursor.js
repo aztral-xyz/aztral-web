5454bffe1f8bef4eaa98a601e718ee8a788d6eb8
aztral-xyz <njknujk@gmail.com>
2025-03-22 02:30:11 +0200

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,33 +1,5 @@
-class DNACursor {
-    constructor() {
-        this.cursor = document.getElementById('dna-cursor');
-        this.initCursorTracking();
-        this.initTrailEffect();
-    }
-
-    initCursorTracking() {
-        document.addEventListener('mousemove', (e) => {
-            gsap.to(this.cursor, {
-                x: e.clientX,
-                y: e.clientY,
-                duration: 0.1,
-                ease: "power2.out"
-            });
-        });
-    }
-
-    initTrailEffect() {
-        setInterval(() => {
-            const trail = document.createElement('div');
-            trail.className = 'cursor-trail';
-            document.body.appendChild(trail);
-            gsap.to(trail, {
-                x: this.cursor.offsetLeft,
-                y: this.cursor.offsetTop,
-                opacity: 0,
-                duration: 1,
-                onComplete: () => trail.remove()
-            });
-        }, 50);
-    }
-}
+const cursor = document.querySelector(".custom-cursor");
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+});
